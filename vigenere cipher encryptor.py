521016dfc25de0768811e69869f002ba2c42e531
xharlesd <bernidocharlesdavid@gmail.com>
2023-04-21 16:29:02 +0800

    try-again function

--- vigenere cipher encryptor.py
+++ vigenere cipher encryptor.py
@@ -8,8 +8,32 @@ from colorama import Fore, Style
 import time
 
 # Create a try-again function to use the program again if the user wants to 
-    # the program will run again if the user inputted YES
-    # the program will terminate if the user inputted NO
+def try_again():
+    retry = None
+    while retry is None:
+        time.sleep(1)
+        again = input(Fore.MAGENTA + "\n\t    Do you want to try again? (YES/NO)  ")
+
+        # the program will run again if the user inputted YES
+        if again == "Y" or again == "Yes" or again == "YES" or again == "yes":
+            retry = str(again)
+            main()
+
+        # the program will terminate if the user inputted NO
+        if again == "N" or again == "No" or again == "NO" or again == "no":
+            time.sleep(0.5)
+            print(Fore.CYAN + "\t    [Program will be terminated..............]")
+
+            time.sleep(1.5)
+            print("\n")
+            print(Fore.GREEN + "\033[1m-" * 140 + '\033[0m')
+            print(" ")
+
+            lab = pyfiglet.figlet_format("   THANK YOU !!   ", font = "banner3",  width = 140, justify = "center")
+            print(Style.BRIGHT + Fore.GREEN + lab)
+            exit()
+        else:
+            print(Fore.RED + "\t    [ERROR] Please choose either YES or NO only.")
 
 # Create encryption function
     # Loop through each char in message.
