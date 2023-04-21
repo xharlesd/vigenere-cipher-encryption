853fe39bcdca3fdaa5166631c6859e1d3dc0bca1
xharlesd <bernidocharlesdavid@gmail.com>
2023-04-21 16:27:04 +0800

    pseudocode

---
+++ vigenere cipher encryptor.py
@@ -0,0 +1,28 @@
+# Bernido, Charles David P. | BSCPE 1-5
+# Object Oriented Programming | Lab Exercise 1 - Problem 3
+
+# import modules
+
+# Create a try-again function to use the program again if the user wants to 
+    # the program will run again if the user inputted YES
+    # the program will terminate if the user inputted NO
+
+# Create encryption function
+    # Loop through each char in message.
+    # Move to the next letter in the key.
+
+# Use pyfiglet formatting to "Lab. Exercise No. 1"
+# format introductory message
+
+# insert time delay
+
+# define main function
+    # ask the user to input the message and key
+    # accept only uppercase letters and no spaces
+    # call encrypt function
+    # display cipher text
+
+# call main function
+
+# Message: THISISTHELASTTASKHOORDAY
+# Key: KNIGHTS
